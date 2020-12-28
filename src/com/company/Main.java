4afc31d385e2528910960d53cee5d3e14ec21db4
akhilsharmaa <akhilsharma.pro@gmail.com>
2020-12-28 17:31:20 +0530

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        powerNumberfunction( 5  ,4);
+    }
+
+    public static void powerNumberfunction(int num , int power) {
+
+        int numD = num ;
+        int j = 0 ;
+
+        for ( int n = 1; n <= power ; n++) {
+
+            System.out.println(j);
+        }
+
+    }
+
+}
